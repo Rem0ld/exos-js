f8b75b70551d9c0a372ab21d4f1886ae4789c291
Rem0ld <60188728+Rem0ld@users.noreply.github.com>
2022-05-05 10:34:02 +0200

    first commit

---
+++ challenges/1. Meeting Availability/challenge.js
@@ -0,0 +1,13 @@
+const moment = require("moment");
+
+const start = moment("2017-02-21T05:00:00-05:00");
+const end = moment("2017-02-21T22:00:00-05:00");
+
+function findFreeTimes(start, end, duration, events) {
+  const availableTimes = [];
+ 
+  return availableTimes;
+}
+
+const availableTimes = findFreeTimes(start, end, 15, data);
+
